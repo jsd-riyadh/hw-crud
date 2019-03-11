0fb1729ad5da5dba1833d5062a2b3749886f2aa3
faisal3397 <faisal.alowaisi.97@gmail.com>
2019-03-11 15:13:00 +0300

    hw solution

--- main.js
+++ main.js
@@ -1,83 +1,108 @@
-$(function(){ //must always be here if you use JQuery
+let collection = { //var collection is collection, what`s inside is document
+
+}
+//CRUD - Create, Read, Update and Delete
+$(function(){
+
+    let db = firebase.firestore().collection('restaurants');
+    let resList = $('.res-container');
     
-    let db = firebase.firestore().collection('restaurants')
-    let resList = $('.res-container')
+    db.get().then(res => {
+       let changes = res.docChanges();// gets array of docs in my collection and checks for changes
 
-    db.get()
-    .then(result => {
-        let changes = result.docChanges()//gets array of docs
-
-        changes.forEach(res => {
-            console.log(res.doc.data());
-            resList.append(`<li data-id="${res.doc.id}">${res.doc.data().name} - ${res.doc.data().location} <button class="edit">edit</button> <button class="delete">delete</button> </li>`)
-
-            
-        });
+       changes.forEach(res => {
+           //Read
+           console.log(res.doc.data());
+           resList.append(`<li data-id="${res.doc.id}">${res.doc.data().name} - ${res.doc.data().location}
+           <button class="edit">Edit</button>
+           <button class="delete">Delete</button>
+           </li>`)
+           
+           
+       })
         
-    }).catch(err => console.log(err))
-
-
-    resList.on('click', ".delete", function(){
+    }).catch(err => {
+        console.log(err);
+        
+    })
+    
+    //Delete
+    resList.on("click", ".delete", function(){ //we used .on so that we can select an element that didn't exist before but existed after loading the page, so we select the parent then we                                              select delete because it didn't exist before
         // $(this).parent().attr("data-id")
-        let id = $(this).parent().data("id") // gets parent data-id
-
-        db.doc(id).delete()
-        // .then()
-
+        let id = $(this).parent().data("id") //Same as the one above
+       
+        db.doc(id).delete().then(() => {
+            $(this).parent().remove();
+        })
+       
     })
 
-    resList.on('click', ".edit", function(){
-        // $(this).parent().attr("data-id")
-        let id = $(this).parent().data("id")
+    //Update
+    resList.on("click", ".edit", function(){ 
+        let id = $(this).parent().data("id") //store the id of the item
 
+  
+        //show a form at the exact position where record is with button for update and cancel
+        $(this).parent().replaceWith('<li><input type="text" name="editedName" placeholder="Name of Restaurant"> <input type="text" name="editedLocation" placeholder="Location of Restaurant"> <button class="update">Update</button> <button class="cancel">Cancel</button></li>')
+       
         db.doc(id).get().then(res => {
-            // console.log(res.data());
-            
-            $('input[name=name]').val(res.data().name)
-            $('input[name=location]').val(res.data().location)
+            console.log(res);
+            //we get the values of the selected item and put it in the edit form
+            $('input[name=editedName]').val(res.data().name);
+            $('input[name=editedLocation').val(res.data().location);
         })
 
+        resList.on("click",".update",function(){
+            let name =  $('input[name=editedName]').val() 
+            let location = $('input[name=editedLocation').val()
+    
+            db.doc(id).update({
+                name: name,
+                location: location
+    
+            }).then(res => {
+                console.log(res);
+                $(this).parent().replaceWith(`<li data-id="${id}">${name} - ${location}
+                <button class="edit">Edit</button>
+                <button class="delete">Delete</button>
+                </li>`)
+            })
+        })
+    
+        resList.on("click",".cancel",function(){
+            //when we click cancel we return to the old item with its old values
+            let name =  $('input[name=editedName]').val()
+            let location = $('input[name=editedLocation').val()
+            $(this).parent().replaceWith(`<li data-id="${id}">${name} - ${location}
+            <button class="edit">Edit</button>
+            <button class="delete">Delete</button>
+            </li>`)
+        })
+        
     })
 
-    //create data and store to restaurants collection
+
+
+    //Create
     $('.submit').click(function(){
-        
-        let name = $('input[name=name]').val()
-        let location = $('input[name=location]').val()
+      let name =  $('input[name=name]').val()
+      let location = $('input[name=location').val()
 
-        console.log(name)
-        console.log(location)
+      console.log(name);
+      console.log(location);
 
-        db.add({
-            name: name,
-            location: location
-        }).then(res =>{
-            resList.append(`<li data-id="${res.id}">${name} - ${location} <button class="edit">edit</button> <button class="delete">delete</button></li>`)
-            
-            //clear the input fields after append
-            $('input[name=name]').val("")
-            $('input[name=location]').val("")
-
-            //alert message
-            $("#alert").css("display", "block")
-            $("#alert").append("Added to List!")
-
-            //set delay 4 seconds then empty alert
-            setInterval(function(){ 
-                $("#alert").css("display", "none")
-                $("#alert").empty() 
-            }, 4000)
-            
-        })
-        
-        
+      db.add({
+          name: name,
+          location: location
+      }).then(res => {
+          console.log(res);
+          resList.append(`<li data-id="${res.id}">${name} - ${location}
+          <button class="edit">Edit</button>
+          <button class="delete">Delete</button>
+          </li>`)
+      })
+      
+      $('input[name=name]').val('')
+      $('input[name=location').val('')
     })
-
-    //Update data
-    //id is parent data-id
-    //selector .parent().data("id")
-    // db.doc(id).update({
-
-        // })
-        // .then()
 })
